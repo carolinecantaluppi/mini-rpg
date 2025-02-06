f4bff4afe69cd22b3a6330f130098f280f307f3f
Caroline Cantaluppi <caroline.cantaluppi@gmail.com>
2025-02-06 15:11:34 +0100

    implementata la logica della funzione turnoGiocatore

--- mini-rpg.js
+++ mini-rpg.js
@@ -1,24 +1,10 @@
-// Altre specifiche:
-// Il giocatore può curarsi solo 3 volte
-// La cura, l’attacco e la difesa sono valori random tra due estremi
-// Le probabilità di fuga calano in base al tipo di mostro (es. 90% con uno slime, 5% con Malakit il Dio Caduto)
-
-// Oltre al giochino dovrai:
-// Gestire il versionamento del codice con l’uso di Git e mantenere una cronologia delle modifiche pulita e con messaggi di commit chiari
-// Salvare la tua repository locale su una repo pubblica su GitHub affinché possa vederla e valutarla
-// Eh sì, qui scatta un voto da 1 a 10. Il voto terrà conto di:
-// Qualità del codice
-// Organizzazione
-// Qualità della cronologia Git 
-
-
 // Creare un videogioco testuale che preveda:
 // Creazione del personaggio (nome scelto dall’utente, vita = 100, attacco e difesa)
 let giocatore = {
     nome: prompt("Inserisci il tuo nome: "),
     vita: 100,
-    attacco: Math.floor(Math.random() * 20) + 10,
-    difesa: Math.floor(Math.random() * 20) + 10,
+    attacco: Math.floor(Math.random() * 9) + 1,
+    difesa: Math.floor(Math.random() * 9) + 1,
     cura: 3,
     fuga: 0
 }
@@ -26,23 +12,33 @@ let giocatore = {
 // Selezione randomica di mostri (nome, vita, attacco e difesa)
 let mostro = {
     nome: "",
-    vita: 0,
-    attacco: 0,
-    difesa: 0
+    vita: Math.floor(Math.random() * 99) + 1,
+    attacco:  Math.floor(Math.random() * 9) + 1,
+    difesa: Math.floor(Math.random() * 9) + 1
 }
 
 
 // Gestire il turno del giocatore (può compiere 3 opzioni: attaccare, curarsi e tentare la fuga)
+// Altre specifiche:
+// Il giocatore può curarsi solo 3 volte
+// La cura, l’attacco e la difesa sono valori random tra due estremi
+// Le probabilità di fuga calano in base al tipo di mostro (es. 90% con uno slime, 5% con Malakit il Dio Caduto)
 function turnoGiocatore() {
-    let scelta = prompt("Scegli una delle seguenti opzioni: attaccare, curarsi o fuga \n"); 
-    if (scelta === "attaccare") {
-        attacco();
-    } else if (scelta === "curarsi") {
-        cura();
-    } else if (scelta === "fuga") {
-        fuga();
-    } else {
-        console.log("Non valido. Scrivi una opzione valida.");
+    let azioni = prompt("Scegli una delle seguenti opzioni: attaccare, curarsi o fuggire"); 
+    while (giocatore.vita > 0 && mostro.vita > 0) {
+        if (azioni === "attaccare") {
+            let danno = Math.floor(Math.random() * giocatore.attacco + 5);
+            mostro.vita = mostro.vita - danno;
+            console.log(`Hai attaccato il mostro! Vita rimanente del mostro: ${mostro.vita}`);
+        } else if (azioni === "curarsi") {
+            giocatore.vita = giocatore.vita + Math.floor(Math.random() * 10 + 5);
+            giocatore.vita--;
+            console.log(`Ti hai curato! Vita aumentata a: ${giocatore.vita}`);
+        } else if (azioni === "fuggire") {
+            console.log("Sei riuscito a fuggire.");
+        } else {
+            console.log("Non valido. Scrivi una opzione valida.");
+        }
     }
 }
         
@@ -66,3 +62,4 @@ function fineGioco() {
         console.log("Hai fuggito! Hai evitato il mostro.");
     }
 }
+
